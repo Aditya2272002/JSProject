c44bff0511c5d3693d7a2e4e360e6342a7d7e89b
Aditya Maurya <adiutube22@gmail.com>
2021-06-23 12:19:46 +0530

    Version 1.1

--- index.js
+++ index.js
@@ -7,21 +7,13 @@
 // Note:- 'dot' in querySelector 's ("class")
 const taskContainer = document.querySelector(".task__container");
 
+//step 5 :- stroring the newTask in this array
+//         It will be stored as [{} , {}] -> [jsObject1, jsObject2...]
+let globalStore = [];
 
-//step 1 :- storing user input in a variable directly using -> .value
-//          and geting user input using getElementById("<__id__>")
-const saveChanges  = () => {
-    const taskData = {   // js object
-        id: `${Date.now()}`,
-        imageUrl : document.getElementById("imageurl").value,
-        taskTitle: document.getElementById("tasktitle").value,
-        taskType: document.getElementById("taskType").value,
-        taskDescription: document.getElementById("taskDescription").value,
-    };
-
-    //step 3 :- create a constant that stores the HTML code along with getting 
+ //step 3 :- create a constant that stores the HTML code along with getting 
     //          replaced by input (using template literals) given by user that are stored in Step 1.
-    const newCard = `
+    const newCard = (taskData) => `
     <div class="col-md-6 col-lg-4" id=${taskData.id}>
     <div class="card">
       <div class="card-header d-flex justify-content-end gap-2">
@@ -46,8 +38,62 @@ const saveChanges  = () => {
   </div>
   `;
 
+//step 8:- when we add items after refereshing the browser the localStorage replace the previous 
+//         data with new, this is because of globlaStore is becoming empty when we reload.
+//         So we need to load data in Object format in globalStore at initialization.
+const loadInitialData = () =>{
+  // -> Access localStorage 
+  const getCardData = localStorage.getItem("tasky");
+
+  // ->Convert to Object from string as array globalStore only store Object
+  const {cards} = JSON.parse(getCardData);
+
+  // ->loop over the array and create HTML card and inhject it to DOM
+  cards.map((cardObject) => {
+    taskContainer.insertAdjacentHTML("beforeend",newCard(cardObject));
+
+  // -> update globalStore
+    globalStore.push(cardObject);
+  })
+};
+
+
+//step 1 :- storing user input in a variable directly using -> .value
+//          and geting user input using getElementById("<__id__>")
+const saveChanges  = () => {
+    const taskData = {   // js object
+        id: `${Date.now()}`,
+        imageUrl : document.getElementById("imageurl").value,
+        taskTitle: document.getElementById("tasktitle").value,
+        taskType: document.getElementById("taskType").value,
+        taskDescription: document.getElementById("taskDescription").value,
+    };
+
+   
+
   //step 4:- inserting data to parent element using 
   //         insertAdjacentHTML( <__where to insert__>, <__what to insert__> )
-  taskContainer.insertAdjacentHTML("beforeend",newCard);
+  taskContainer.insertAdjacentHTML("beforeend",newCard(taskData));
+
+  // Step 6:- pushing raw data to globalStore
+  globalStore.push(taskData);
+
+  //Step 7:-Accessing "localStorage" and Storing to local Database using ".setItem"
+  // setItem takes -> ("key to access localStorage", "what to store?")
+
+    // In above why we storing the data first in globalStore and then pushing to localStorage?
+    //     localStorage.setItem("tasky",taskData);
+    // Answer:-
+    // This is because when we directly push taskData ,so on running the browser multiple 
+    // times it replaces the data not adds it in localStorage. 
+
+   // localStorage.setItem("tasky",globalStore); // -> This gives Object but we want that String 
+                                               //    to br stored in localStorage.
+
+    localStorage.setItem("tasky",JSON.stringify({cards:globalStore}));
+    // so to convert to string use stringify and to organise the data in easy-to-access-manner
+    // we used JSON ,  now JSON.stringify() , takes object as it's parameter so we created
+    // a object card and storing all string in it.
+
 
 };
